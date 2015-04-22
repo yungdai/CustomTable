5d9c5beed9d8422c92c8e3fc7e6d7954f3a5b773
Yung Dai <yungchidai@gmail.com>
2015-04-22 14:04:44 -0400

    starting of Chapter 15 to change the images to a collection view

---
+++ RecipeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.m
+//  CustomTable
+//
+//  Created by Yung Dai on 2015-04-22.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import "RecipeCollectionViewCell.h"
+
+@implementation RecipeCollectionViewCell
+
+@end
